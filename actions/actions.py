d0c0110bf1b1640b3a8d460b8b88ed829e8cd7b8
Sanlee Sa <sanlee.r.sa@gmail.com>
2021-05-24 17:19:44 +0530

    added level_1 menu from db

--- actions/actions.py
+++ actions/actions.py
@@ -3,7 +3,10 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
+from logging import NullHandler
+import re
 from typing import Any, Text, Dict, List
+from dns.rdatatype import NULL
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import AllSlotsReset, SlotSet
@@ -11,6 +14,9 @@ from rasa_sdk.events import AllSlotsReset, SlotSet
 from databaseconnection import DatabaseConnection as dbcon
 
 class ActionHandleQuery(Action):
+
+    db = dbcon()
+
     def name(self):
         return 'action_handle_query'
         
@@ -20,12 +26,37 @@ class ActionHandleQuery(Action):
             degree = tracker.get_slot('degree')
             course = tracker.get_slot('course')
             query = tracker.get_slot('query')
-            dispatcher.utter_message(text=f"program={program}, degree={degree}, course={course}, query={query}")
+            results = self.db.get_query_response(program, degree, course, query)
+            dispatcher.utter_message(text=results[0][0])
+        except Exception as ex:
+            print(ex)
+        return []
+
+class ActionAskProgram(Action):
+
+    db = dbcon()
+
+    def name(self):
+        return 'action_ask_program'
+        
+    def run(self, dispatcher, tracker, domain):
+        try:
+            buttons = []
+            results = self.db.get_program_list()
+            if (results != []):
+                for list in results:
+                    buttons.append({"title": list[0], "payload": list[1]})
+                dispatcher.utter_message(text="Hi, what program are you looking for?", buttons=buttons)
+            else:
+                dispatcher.utter_message(text="Sorry I couldn't find any programs")
         except Exception as ex:
             print(ex)
         return []
 
 class ActionAskDegree(Action):
+
+    db = dbcon()
+
     def name(self):
         return 'action_ask_degree'
         
@@ -36,7 +67,7 @@ class ActionAskDegree(Action):
             if (program in ["diploma", "postgraduatediploma", "phd"]):
                 ret.append(SlotSet('course', 'na'))
             buttons = []
-            results = dbcon().get_degree_list(str(program))
+            results = self.db.get_degree_list(program)
             for list in results:
                 buttons.append({"title": list[0], "payload": list[1]})
             dispatcher.utter_message(text="Please select a degree:", buttons=buttons)
@@ -45,6 +76,9 @@ class ActionAskDegree(Action):
         return ret
 
 class ActionAskCourse(Action):
+
+    db = dbcon()
+
     def name(self):
         return 'action_ask_course'
         
@@ -56,10 +90,34 @@ class ActionAskCourse(Action):
             if (program in ["diploma", "postgraduatediploma", "phd"]):
                 ret.append(SlotSet('course', 'na'))
             buttons = []
-            results = dbcon().get_course_list(str(program), str(degree))
+            results = self.db.get_course_list(program, degree)
             for list in results:
                 buttons.append({"title": list[0], "payload": list[1]})
             dispatcher.utter_message(text="Please select a course:", buttons=buttons)
+        except Exception as ex:
+            print(ex)
+        return ret
+
+class ActionAskQuery(Action):
+
+    db = dbcon()
+    
+    def name(self):
+        return 'action_ask_query'
+        
+    def run(self, dispatcher, tracker, domain):
+        try:
+            ret = []
+            program = tracker.get_slot('program')
+            degree = tracker.get_slot('degree')
+            course = tracker.get_slot('course')
+            if (program in ["diploma", "postgraduatediploma", "phd"]):
+                ret.append(SlotSet('course', 'na'))
+            buttons = []
+            results = self.db.get_query_list(str(program), str(degree), str(course))
+            for list in results:
+                buttons.append({"title": list[0], "payload": list[1]})
+            dispatcher.utter_message(text="What is your query pertaining to:", buttons=buttons)
         except Exception as ex:
             print(ex)
         return ret
--- databaseconnection.py
+++ databaseconnection.py
@@ -27,17 +27,18 @@ class DatabaseConnection():
         val = user_query
         my_cursor.execute(sql, (val,))
         results = my_cursor.fetchall()
-        print(f"The bot found this as a response: {results}")
+        print(f"found this as a response: {results}")
         return results
 
     def get_degree_list(self, program):
         try:
             my_cursor = self.cursor
-            sql = "SELECT level_2.title,level_2.payload FROM level_2 INNER JOIN level_1 on level_1.level_1_id = level_2.level_1_id_fk WHERE level_1.payload = (%s);"
+            sql = """SELECT level_2.title,level_2.payload FROM level_2 
+            INNER JOIN level_1 on level_1.level_1_id = level_2.level_1_id_fk WHERE level_1.payload = (%s);"""
             val = program
             my_cursor.execute(sql, (val,))
             results = my_cursor.fetchall()
-            print(f"The bot found this as a response: {results}")
+            print(f"found this as a response: {results}")
             return results
         except Exception as err:
             print("Err:",err)
@@ -45,26 +46,55 @@ class DatabaseConnection():
     def get_course_list(self, program, degree):
         try:
             my_cursor = self.cursor
-            sql = "SELECT level_3.title, level_3.payload FROM level_3 INNER JOIN level_2 on level_3.level_2_id_fk = level_2.level_2_id INNER JOIN level_1 on level_2.level_1_id_fk = level_1.level_1_id WHERE level_1.payload = (%s) AND level_2.payload = (%s);"
-            pro = program
-            deg = degree
-            my_cursor.execute(sql, (pro, deg))
+            sql = """SELECT level_3.title, level_3.payload FROM level_3 
+            INNER JOIN level_2 on level_3.level_2_id_fk = level_2.level_2_id 
+            INNER JOIN level_1 on level_2.level_1_id_fk = level_1.level_1_id 
+            WHERE level_1.payload = (%s) AND level_2.payload = (%s);"""
+            my_cursor.execute(sql, (program, degree))
             results = my_cursor.fetchall()
-            print(f"The bot found this as a response: {results}")
+            print(f"found this as a response: {results}")
             return results
         except Exception as err:
             print("Err:",err)
     
-    # def get_query_list(self, program, degree):
-    #     try:
-    #         my_cursor = self.cursor
-    #         sql = "SELECT level_3.title, level_3.payload FROM level_3 INNER JOIN level_2 on level_3.level_2_id_fk = level_2.level_2_id INNER JOIN level_1 on level_2.level_1_id_fk = level_1.level_1_id WHERE level_1.payload = (%s) AND level_2.payload = (%s);"
-    #         pro = program
-    #         deg = degree
-    #         my_cursor.execute(sql, (pro, deg))
-    #         results = my_cursor.fetchall()
-    #         print(f"The bot found this as a response: {results}")
-    #         return results
-    #     except Exception as err:
-    #         print("Err:",err)
+    def get_query_list(self, program, degree, course):
+        try:
+            my_cursor = self.cursor
+            sql = """SELECT level_4.title, level_4.payload FROM level_4 
+            INNER JOIN level_3 on level_4.level_3_id_fk = level_3.level_3_id 
+            INNER JOIN level_2 on level_3.level_2_id_fk = level_2.level_2_id 
+            INNER JOIN level_1 on level_2.level_1_id_fk = level_1.level_1_id 
+            WHERE level_3.payload = (%s) AND level_2.payload = (%s) AND level_1.payload = (%s);"""
+            my_cursor.execute(sql, (course, degree, program))
+            results = my_cursor.fetchall()
+            print(f"found this as a response: {results}")
+            return results
+        except Exception as err:
+            print("Err:",err)
     
+    def get_query_response(self, program, degree, course, query):
+        try:
+            my_cursor = self.cursor
+            sql = """SELECT details FROM level_5 
+            INNER JOIN level_4 ON level_4.level_4_id = level_5.level_4_id_fk 
+            INNER JOIN level_3 ON level_3.level_3_id = level_4.level_3_id_fk 
+            INNER JOIN level_2 ON level_2.level_2_id = level_3.level_2_id_fk 
+            INNER JOIN level_1 on level_1.level_1_id = level_2.level_1_id_fk 
+            WHERE level_1.payload = (%s) AND  level_2.payload = (%s) AND  level_3.payload = (%s) AND level_4.payload = (%s);"""
+            my_cursor.execute(sql, (program , degree, course, query))
+            results = my_cursor.fetchall()
+            print(f"found this as a response: {results}")
+            return results
+        except Exception as err:
+            print("Err:",err)
+
+    def get_program_list(self):
+        try:
+            my_cursor = self.cursor
+            sql = """SELECT title, payload FROM level_1;"""
+            my_cursor.execute(sql)
+            results = my_cursor.fetchall()
+            print(f"found this as a response: {results}")
+            return results
+        except Exception as err:
+            print("Err:",err)
